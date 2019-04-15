2ca588f53dc26793f2a04638e046e18a01151872
Seth Phillips <sethphillips042@gmail.com>
2019-04-15 18:29:01 -0400

    Routing fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EducationListComponent } from './education-list/education-list.component';
+import { EducationEditComponent } from './education-edit/education-edit.component';
+import { EducationDetailComponent } from './education-detail/education-detail.component';
+import { EducationCreateComponent } from './education-create/education-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EducationListComponent,
+    EducationEditComponent,
+    EducationDetailComponent,
+    EducationCreateComponent
   ],
   imports: [
     BrowserModule
